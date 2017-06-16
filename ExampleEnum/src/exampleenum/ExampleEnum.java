2204974dfc8cb28d64ab83bff41f8be1df58f910
rik <mr.valery85@yandex.ru>
2017-06-16 10:28:31 +0300

    Output all names and correspondent values

--- ExampleEnum/src/exampleenum/ExampleEnum.java
+++ ExampleEnum/src/exampleenum/ExampleEnum.java
@@ -8,7 +8,8 @@ public class ExampleEnum {
         System.out.println("From class  = " + (new MyClassAsEnum("ADD").str) + " "
                 + MyClassAsEnum.ADD.str  );
         System.out.println("From enum = " + MyEnum.ADD + " " + MyEnum.ADD.getValue());
-        
+
+//output all field names of enum and correspondent values        
         for (MyEnum el : MyEnum.values()){
             System.out.println("field name = " + el + 
                                 " value = " + el.getValue());
